eae945b7f96712de4dfdac4c5a914e9b52b99194
Paul Hyman <mail@phyman.com>
2014-10-27 23:30:45 -0500

    seed file set to del and repop user table

--- Gemfile
+++ Gemfile
@@ -27,7 +27,12 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+# Use debugger
+gem 'byebug', group: :development
+
+# Use faker to generate users
+gem 'faker', group: :development
 
 # Use unicorn as the app server
 # gem 'unicorn'
@@ -35,6 +40,4 @@ gem 'spring',        group: :development
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-# Use debugger
-# gem 'debugger', group: [:development, :test]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Global Constant Variables
+GEN_USERS = 10
+
+# ----------------------------------
+
+# * Extending Faker frame *
+# module Faker
+#   class MakeDate < Base
+#     class << self
+# monkey patch 'something'
+#       def something(defaultInt=1)
+#         ...
+#       end
+#     end
+#   end
+# end
+
+# ----------------------------------
+
+# clean all the previous data out of the DB
+def delete_all
+  User.delete_all
+end
+
+# generate a set of users & populate table
+def generate_users
+  @users = (1..GEN_USERS).to_a
+  (1..@users.length).each do |user|
+    username = Faker::Internet.user_name
+    email = Faker::Internet.safe_email(username)
+    unless User.create!(username: username, email: email)
+      puts "*********** ERROR ******************"
+    end
+  end
+end
+
+# ----------------------------------
+
+##
+# delete table contents and re-populate
+delete_all
+generate_users
+
+# eof----------------------------------
